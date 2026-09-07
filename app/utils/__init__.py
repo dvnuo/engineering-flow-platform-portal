488523c4daf2f8dd9b60ed2945d1ea892eb8068c
LucasLai <lan_ff@qq.com>
2026-09-07 11:47:43 +0800

    Add login_user_by_code to module exports

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,5 @@
 from app.utils.naming import runtime_names, to_k8s_name
 from app.utils.state_machine import VALID_STATUSES, can_transition, is_valid_status
+from app.utils.sso_auth import login_user_by_code
 
-__all__ = ["to_k8s_name", "runtime_names", "VALID_STATUSES", "can_transition", "is_valid_status"]
+__all__ = ["to_k8s_name", "runtime_names", "VALID_STATUSES", "can_transition", "is_valid_status", "login_user_by_code"]
